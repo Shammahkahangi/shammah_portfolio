82549129d805b2df6de4c9c47e38acc0605994c7
Shammahkahangi <shammahkahangi@gmail.com>
2024-01-06 00:52:29 +0300

    final commit

---
+++ src/Pages/About.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import CTA from '../components/CTA';
+import { skills, experiences } from '../constants';
+
+const About = () => {
+  return (
+    <section className="max-container">
+
+
+      <div className='relative mb-5 '>
+      <img src="src\assets\images\IMG_8736.jpeg" alt="Shammah Kahangi" className="w-[400px] h-[400px] max-h-90 object-cover rounded-md border-4 border-gray-500" />
+      </div>
+      <h1 className="headtext">
+        Hello, I'm<span className="blue-gradient_text font-semibold drop-shadow"> SHAMMAH KAHANGI</span>
+      </h1>
+
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+        I'm a dedicated Full Stack Developer and UI Designer in Uganda, specializing in JavaScript and TypeScript. With a broad skill set covering both front-end and back-end development, I'm passionate about creating dynamic and responsive web applications. My commitment to delivering exceptional user experiences is demonstrated through the seamless integration of design aesthetics and functionality. I thrive on solving complex challenges and stay at the forefront of evolving technologies. Let's collaborate to bring your digital vision to life with innovation and precision.</p>      </div>
+
+      <div className="py-10 flex flex-col">
+        <h3 className="subhead-text"> My skills</h3>
+        <div className="mt-16 flex flex-wrap gap-12">
+          {skills.map((skill, index) => (
+            <div key={`skill-${index}`} className=" block-container w-20 h-20">
+              <div className="btn-back rounded-xl" />
+              <div className="btn-front rounded-xl flex justify-center items-center">
+                <img src={skill.imageUrl} alt={skill.name} className="w-1/2 h-1/2 object-contain" />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="py-16">
+        <h3 className="subhead-text">Work Experience</h3>
+        <div className="mt-5 flex flex-col gap-3 text-slate-500">
+          <p>
+            I've worked with all sorts of companies, leveling up my skills and teaming up with smart people. Here are
+            some highlights
+          </p>
+        </div>
+
+        <div className="mt-12 flex">
+          <VerticalTimeline>
+            {experiences.map((experience, index) => (
+              <VerticalTimelineElement
+                key={`experience-${index}`}
+                className="w-[60%] h-[60%] object-contain"
+                date={experience.date}
+                icon={
+                  <div className="flex justify-center items-center w-full h-full">
+                    <img src={experience.icon} alt={experience.company_name} />
+                  </div>
+                }
+                iconStyle={{
+                  background: experience.bg,
+                }}
+                contentStyle={{
+                  borderBottom: '8px',
+                  borderStyle: 'solid',
+                  borderBottomColor: experience.iconBg,
+                  boxShadow: 'none',
+                }}
+              >
+                <div>
+                  <h3 className="text-black text-xl font-poppins font-semibold">{experience.title}</h3>
+                  <p className="text-black-500 font-medium font-base" style={{ margin: 0 }}>
+                    {experience.company_name}
+                  </p>
+
+                  <ul className="my-5 list-disc ml-5 space-y-2">
+                    {experience.points.map((point, pointIndex) => (
+                      <li
+                        key={`experience-${experience.company_name}-point-${pointIndex}`}
+                        className="text-black-500/50 font-normal pl-1 text-sm"
+                      >
+                        {point}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
+      </div>
+
+      <hr className="border-slate-200" />
+
+      <CTA />
+    </section>
+  );
+};
+
+export default About;
---
+++ src/constants/index.js
@@ -0,0 +1,206 @@
+import {  iconsdiamond ,shopify, starbucks, tesla, iconskkey} from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    twitterx,
+    typescript,
+   
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "React.js Developer",
+        company_name: "KKTARI PROCUMENT LTD",
+        
+        icon: iconskkey,
+        date: "March 2023 - to date",
+        points: [
+            "Develop and maintain web applications for a procurement company using React.js and associated technologies.",
+            "Collaborate with cross-functional teams, including designers, product managers, and developers, to create high-quality products tailored for the procurement industry.",
+            " Implement responsive design principles and ensure cross-browser compatibility for optimal user accessibility.",
+            "Participate in code reviews, providing constructive feedback to enhance the quality and efficiency of procurement solutions.",
+        ],
+    },
+    {
+        title: "React Native Developer",
+        company_name: "Diamond Credit Ltd",
+        icon: iconsdiamond,
+        iconBg: "#fbc3bc",
+        date: "Jan 2021 - Feb 2022",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies. for a money lending institution",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    
+    
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/Shammahkahangi',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/shammah-kahangi-gordon/',
+    },
+    {
+        name: 'twitter',
+        iconUrl: twitterx,
+        link: "https://twitter.com/kahangi_shammah"
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: estate,
+        theme: 'btn-back-red',
+        name: 'Administration Dashboard',
+        description: 'Created an Admin Dashboard for tracking and notifying users of price changes in online products. The dashboard includes tables, charts, and a Kanban board for streamlined management and decision-making.',
+        link: 'https://github.com/Shammahkahangi/enterprise.git',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Full Stack Event Application',
+        description: 'Created a full-stack Events application using clerk and shadcnui helping users advertise their events and connect with a community of people with shared interests',
+        link: 'https://github.com/Shammahkahangi/Evently.git',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'Light Company Landing Page',
+        description: 'Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.',
+        link: 'https://github.com/Shammahkahangi/Light-company.git',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'three-js portfolio',
+        description: 'Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.',
+        link: 'https://github.com/Shammahkahangi/shammahs_portfolio.git',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'Diamond Credit Website',
+        description: 'Designed a sleek website for Diamond Finance, simplifying the borrowing process. Streamlined interface for easy loan exploration and application. Ensured transparency on rates and eligibility, prioritizing user security. Elevate your financial journey with Diamond Finance intuitive online platform.',
+        link: 'https://github.com/Shammahkahangi/Diamond-Finance.git',
+    },
+    
+    
+];
